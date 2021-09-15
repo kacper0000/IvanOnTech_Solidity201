bafe83b4f5825a4c9b61467422aa63d7bb15c0e9
kacper0000 <57183004+kacper0000@users.noreply.github.com>
2021-09-15 17:30:27 +0100

    simple_Array.sol

---
+++ storage_Patterns/simple_Array.sol
@@ -0,0 +1,25 @@
+ // SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.0;
+
+contract simpleList {
+
+  struct EntityStruct {
+    address entityAddress;
+    uint entityData;
+  }
+
+  EntityStruct[] public entityStructs;
+
+  function newEntity(address entityAddress, uint entityData) public returns(EntityStruct memory) {
+    EntityStruct memory newEntity;
+    newEntity.entityAddress = entityAddress;
+    newEntity.entityData    = entityData;
+    entityStructs.push(newEntity);
+    return entityStructs[entityStructs.length - 1];
+  }
+
+  function getEntityCount() public view returns(uint entityCount) {
+    return entityStructs.length;
+  }
+}
